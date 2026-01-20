fec1e705c66c522e8bd526ef46ee1d4bb1068b9c
Han <hkjbrian@gmail.com>
2026-01-20 16:21:02 +0900

    feat: BaseTimeEntity 생성 (#1)
    JpaAuditing 에 관련한 3가지 컬럼을 추상 BaseEntity 클래스로 구현

---
+++ src/main/java/com/raisedeveloper/server/domain/common/domain/SoftDeleteEntity.java
@@ -0,0 +1,41 @@
+package com.raisedeveloper.server.domain.common.domain;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class SoftDeleteEntity {
+
+	@CreatedDate
+	@Column(name = "created_at", nullable = false, updatable = false)
+	private LocalDateTime createdAt;
+
+	@LastModifiedDate
+	@Column(name = "updated_at", nullable = false)
+	private LocalDateTime updatedAt;
+
+	@Column(name = "deleted_at")
+	private LocalDateTime deletedAt;
+
+	public boolean isDeleted() {
+		return deletedAt != null;
+	}
+
+	public void softDelete(LocalDateTime now) {
+		if (deletedAt == null) {
+			this.deletedAt = Objects.requireNonNull(now);
+		}
+	}
+}
+
