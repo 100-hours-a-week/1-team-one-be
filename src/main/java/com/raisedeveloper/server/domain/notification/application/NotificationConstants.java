a6b71b55469d2ccb54e9c10f0fe5c22595c81b37
Han <hkjbrian@gmail.com>
2026-01-28 18:50:48 +0900

    feat: notification 생성 (#57)
    - 세션 성공 / 실패 시 각각 Notification 데이터 생성 및 저장

---
+++ src/main/java/com/raisedeveloper/server/domain/notification/application/NotificationConstants.java
@@ -0,0 +1,10 @@
+package com.raisedeveloper.server.domain.notification.application;
+
+public final class NotificationConstants {
+
+	public static final String STRETCHING_SUCCESS = "스트레칭에 성공하였습니다";
+	public static final String STRETCHING_FAILED = "스트레칭에 실패하였습니다";
+
+	private NotificationConstants() {
+	}
+}
