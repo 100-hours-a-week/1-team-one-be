80e03ab9c1d757c8391acc34f0798b88ed0ef57d
Han <hkjbrian@gmail.com>
2026-02-11 17:11:38 +0900

    fix: 영속성 컨텍스트로 인하여 delete 순서 미보장으로 발생하는 오류 해결(#121)
    - flushAutomatically 를 통해 delete 메서드를 호출했을 때 즉시 flush 되도록 설정
    - clearAutomatically 를 통해서 삭제된 엔티티에 대해서 detach 되도록 설정

--- src/main/java/com/raisedeveloper/server/domain/post/infra/PostImageRepository.java
+++ src/main/java/com/raisedeveloper/server/domain/post/infra/PostImageRepository.java
@@ -3,6 +3,9 @@ package com.raisedeveloper.server.domain.post.infra;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.raisedeveloper.server.domain.post.domain.PostImage;
 
@@ -10,5 +13,7 @@ public interface PostImageRepository extends JpaRepository<PostImage, Long> {
 
 	List<PostImage> findByPostIdOrderBySortOrderAsc(Long postId);
 
-	void deleteAllByPostId(Long postId);
+	@Modifying(clearAutomatically = true, flushAutomatically = true)
+	@Query("DELETE FROM PostImage pi WHERE pi.post.id = :postId")
+	void deleteAllByPostId(@Param("postId") Long postId);
 }
