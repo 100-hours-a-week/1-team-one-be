489f12b79924b69c0c54377c598b7ca74eaab955
Han <hkjbrian@gmail.com>
2026-02-12 09:32:06 +0900

    refactor: NotificationReadRequest DTO 수정 (#136)
    - 읽은 Notification 중 가장 상단 (최신) id 와 하단 (오래된) id 를 받도록 수정

--- src/main/java/com/raisedeveloper/server/domain/notification/dto/NotificationReadRequest.java
+++ src/main/java/com/raisedeveloper/server/domain/notification/dto/NotificationReadRequest.java
@@ -1,13 +1,11 @@
 package com.raisedeveloper.server.domain.notification.dto;
 
-import java.time.LocalDateTime;
-
-import com.fasterxml.jackson.annotation.JsonFormat;
-
 import jakarta.validation.constraints.NotNull;
 
 public record NotificationReadRequest(
 	@NotNull
-	LocalDateTime lastNotificationTime
+	Long oldestNotificationId,
+	@NotNull
+	Long latestNotificationId
 ) {
 }
