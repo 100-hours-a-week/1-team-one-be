095a4b95b32e01b2b25ef7cab3792045d66788f0
Han <hkjbrian@gmail.com>
2026-02-27 13:40:38 +0900

    feat: KafkaTopic 설정 (#184)

---
+++ src/main/java/com/raisedeveloper/server/domain/exercise/event/ExerciseKafkaTopics.java
@@ -0,0 +1,9 @@
+package com.raisedeveloper.server.domain.exercise.event;
+
+public final class ExerciseKafkaTopics {
+
+	public static final String SESSION_COMPLETED = "exercise.session.completed";
+
+	private ExerciseKafkaTopics() {
+	}
+}
