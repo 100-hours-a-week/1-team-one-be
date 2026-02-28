19fe3a37698a0b51bce5bfb73e30e795856c11f1
Han <hkjbrian@gmail.com>
2026-02-28 17:14:47 +0900

    fix: 눈 운동 limit 타임이 AI 응답 그대로 저장될 수 있도록 수정 (#195)

--- src/main/java/com/raisedeveloper/server/domain/routine/application/RoutineService.java
+++ src/main/java/com/raisedeveloper/server/domain/routine/application/RoutineService.java
@@ -156,7 +156,7 @@ public class RoutineService {
 					exercise,
 					stepDto.targetReps() != null ? stepDto.targetReps().shortValue() : null,
 					stepDto.durationTime() != null ? stepDto.durationTime().shortValue() : null,
-					EYES.equals(stepDto.type()) ? 0 : stepDto.limitTime().shortValue(),
+					stepDto.limitTime().shortValue(),
 					stepDto.stepOrder().shortValue()
 				);
 				routineStepRepository.save(step);
