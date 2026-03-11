5d69e0ac77a73df56c9b28364a8fec53b282d5e3
Han <hkjbrian@gmail.com>
2026-03-11 20:35:10 +0900

    feat: ExerciseSatisfaction 프로젝션을 통한 조회 로직 추가 (#231)

--- src/main/java/com/raisedeveloper/server/domain/satisfaction/infra/ExerciseSatisfactionRepository.java
+++ src/main/java/com/raisedeveloper/server/domain/satisfaction/infra/ExerciseSatisfactionRepository.java
@@ -4,6 +4,7 @@ import java.util.Collection;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.raisedeveloper.server.domain.satisfaction.domain.ExerciseSatisfaction;
@@ -12,4 +13,22 @@ import com.raisedeveloper.server.domain.satisfaction.domain.ExerciseSatisfaction
 public interface ExerciseSatisfactionRepository extends JpaRepository<ExerciseSatisfaction, Long> {
 
 	List<ExerciseSatisfaction> findAllByUserIdAndExerciseIdIn(Long userId, Collection<Long> exerciseIds);
+
+	@Query("""
+		SELECT
+			es.user.id AS userId,
+			es.exercise.id AS exerciseId,
+			es.satisfaction AS satisfaction
+		FROM ExerciseSatisfaction es
+		ORDER BY es.id ASC
+		""")
+	List<ExerciseSatisfactionSyncProjection> findAllForAiSync();
+
+	interface ExerciseSatisfactionSyncProjection {
+		Long getUserId();
+
+		Long getExerciseId();
+
+		Byte getSatisfaction();
+	}
 }
